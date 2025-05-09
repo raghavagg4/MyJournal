675df75bebe9033822564ac844db343f8763f263
Raghav Aggarwal <raghav4@terpmail.umd.edu>
2025-05-09 15:07:19 -0400

    production fix

--- Journal/flask_app/config.py
+++ Journal/flask_app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-SECRET_KEY = os.urandom(24)
+SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
 MONGODB_HOST = os.getenv('MONGODB_HOST')
 MONGODB_SETTINGS = {
     'host': MONGODB_HOST,
